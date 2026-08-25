0d3c6229dedc7a038a878fdf7d8aae669a2e3bd2
Illan Fernandez <illanes.f.m@gmail.com>
2026-08-25 12:16:30 -0300

    cambios img

--- sections-b.jsx
+++ sections-b.jsx
@@ -6,12 +6,12 @@ const SERVICIOS = [
     {
     id: "coworking", t: "Co-working", bg: "var(--turquesa)",
     p: "Un escritorio físico, buen wi fi, buen café y un entorno lleno de personas creando proyectos con propósito como vos. Venís cuando querés.",
-    cta: "Reservar", placeholder: "Foto de coworking", img: "img/CoworkingWeek-53.jpg",
+    cta: "Reservar", placeholder: "Foto de coworking", img: "img/coworking.jpg",
   },
   {
     id: "coliving", t: "Co-living", bg: "var(--magenta)",
     p: "Un cuarto en una de las casas. Vivís, creás y compartís espacio con una comunidad que te sostiene e impulsa día a día.",
-    cta: "Conocer más", placeholder: "Foto de coliving", img: "img/coliving.jpg",
+    cta: "Conocer más", placeholder: "Foto de coliving", img: "img/coliving-2.jpg",
   },
   {
     id: "experiencias", t: "Experiencias", bg: "var(--ambar)",
