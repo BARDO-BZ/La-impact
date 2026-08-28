136f0e024e81e20a0b6147ea111611812c40014b
Illan Fernandez <illanes.f.m@gmail.com>
2026-08-28 12:26:15 -0300

    cambio navbar

--- sections-a.jsx
+++ sections-a.jsx
@@ -14,7 +14,7 @@ function Nav({ onCTA }) {
         <div className="nav__links">
           <a className="txtlink" href="#coworking">Coworking</a>
           <a className="txtlink" href="#coliving">Coliving</a>
-          <a className="txtlink" href="#agenda">Experiencias</a>
+          <a className="txtlink" href="#experiencias">Experiencias</a>
         </div>
         <Btn variant="ghost" sm className="nav__cta" href={FORM_URL} target="_blank" rel="noopener noreferrer">Sumate</Btn>
       </div>
